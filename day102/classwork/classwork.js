daba9bb1add97d8c7ed3f235c2529d0357ee6bee
tato-mchedlidze <tatomchedlidze210@gmail.com>
2025-09-24 14:56:21 +0400

    second commit

--- day102/classwork/classwork.js
+++
@@ -1,7 +0,0 @@
-// cw1
-// cw2 
-function showDay() {
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let today = new Date().getDay();
-    document.getElementById("myParagraph").textContent = days[today];
-}
